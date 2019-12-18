ecbea850498b6b7278104dad228df82aca89ef04
Mitchell-Johnson <mitchj9@uw.edu>
2019-12-17 18:41:15 -0800

    a

---
+++ script.js
@@ -0,0 +1,3 @@
+var length = prompt("Enter a password length (8-128): ");
+var charType = prompt("Enter character password character type (special, numeric, uppercase, lowercase): ");
+console.log("U+0020");
